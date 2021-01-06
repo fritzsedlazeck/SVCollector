c7d341d8e3e62a619274e8a22eb0a593ba734fe3
Mike Schatz <mschatz@cs.jhu.edu>
2021-01-06 00:10:01 -0500

    Update to disable min allele frequency

--- SVCollector.sh
+++ SVCollector.sh
@@ -7,7 +7,7 @@ then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SVC=$DIR/src/SVCollector 
+SVC=$DIR/Debug/SVCollector 
 PLOT=$DIR/vis/SVCollectorPlot.R
 
 VCF=$1
@@ -24,11 +24,11 @@ echo "Analyzing $VCF"
 echo " computing greedy selection (disable min allele freq)"
 $SVC greedy $VCF -1 $NUMTOPLOT 0 $OUTDIR/$VCFB.greedy >& $OUTDIR/$VCFB.greedy.log
 
-echo " computing topN selection"
-$SVC topN $VCF $NUMTOPLOT $OUTDIR/$VCFB.topN >& $OUTDIR/$VCFB.topN.log
+echo " computing topN selection (disable min allele freq)"
+$SVC topN $VCF $NUMTOPLOT 0 $OUTDIR/$VCFB.topN >& $OUTDIR/$VCFB.topN.log
 
-echo " computing random selection over $RANDOMTRIALS trials "
-(seq $RANDOMTRIALS | parallel -t $SVC random $VCF $NUMTOPLOT $OUTDIR/$VCFB.random.{}) >& $OUTDIR/$VCFB.randomlog
+echo " computing random selection over $RANDOMTRIALS trials (disable min allele freq) "
+(seq $RANDOMTRIALS | parallel -t $SVC random $VCF $NUMTOPLOT 0 $OUTDIR/$VCFB.random.{}) >& $OUTDIR/$VCFB.randomlog
 
 echo " cleaning _tmp files"
 rm -f $OUTDIR/*_tmp
